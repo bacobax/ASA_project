5305a865e374f5be375f1198f275667c4aa55789
BacobaxUnitn <francesco.bassignana@studenti.unitn.it>
2025-06-05 15:43:19 +0200

    dubplication to create single agent

---
+++ implementation_single_agent/src/index.ts
@@ -0,0 +1,23 @@
+import { AgentBDI } from "./libs/agents";
+import { DeliverooApi } from "@unitn-asa/deliveroo-js-client";
+import config from "./config";
+import { playAgents, spawnAgents } from "./libs/utils/multiAgentUtils";
+import { Strategies } from "./libs/utils/common";
+
+
+const strategies = [
+    Strategies.sophisticated,
+    Strategies.linear,
+    Strategies.aggressive,
+]
+// Get number of agents from command line arguments
+const numAgents = process.argv.length > 1 ? parseInt(process.argv[2]) : 3;
+
+const agents = spawnAgents({
+    numAgents: numAgents,
+    tokens: config.tokens,
+    host: config.host,
+    strategies: strategies,
+})
+
+playAgents(agents);
